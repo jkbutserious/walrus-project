82957da2f2105c2f909d437ea06d7d629f4c657d
Janet <janetkarpenske@gmail.com>
2020-06-02 10:27:14 -0700

    Added our own toggle to the image

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
     $(".clickable").click(function() {
       $("#walrus-showing").toggle();
       $("#walrus-hidden").toggle();
+      $("#walrustwo-showing").toggle();
+      $("#walrustwo-hidden").toggle();
     });
   });
